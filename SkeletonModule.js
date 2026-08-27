9c31eaafe63243777768530e747bb8d85de8cbd4
paulviville <paul.viville@gmail.com>
2026-08-27 13:50:58 +0200

    skeleton tested

--- SkeletonModule.js
+++ SkeletonModule.js
@@ -20,22 +20,52 @@ export default class SkeletonModule extends ModuleCore {
 		this.setOnCommand( this.commands.setBones,
 			( { bones } ) => this.setBones( bones )
 		);
+		this.setOnCommand( this.commands.setTransforms,
+			( { boneTransforms } ) => this.setTransforms( boneTransforms )
+		);
 	}
 
 	get bones ( ) {
 		const bonesData = [ ];
-		this.#bones.forEach( 
-			UUID => bonesData.push( { UUID, parent: this.#parents.get( UUID ) } )
-		);
+		for ( const UUID of this.#bones ) {
+			bonesData.push( { UUID, parent: this.#parents.get( UUID ) } )
+		}
 		return bonesData;
 	}
 
 	get transforms ( ) {
-		
+		const boneTransforms = [ ];
+
+		for ( const [ UUID, transform ] of this.#transforms ) {
+			boneTransforms.push( { 
+					UUID,
+					transform: {
+						translation: [ ...transform.translation ],
+						rotation: [ ...transform.rotation ],
+						scale: [ ...transform.scale ],
+					}
+			} );
+		}
+		return boneTransforms;
 	}
 
 	boneTransforms ( boneUUIDs ) {
+		if ( boneUUIDs === undefined )
+			return this.transforms;
 
+		const boneTransforms = [ ];
+		for ( const UUID of boneUUIDs ) {
+			const transform = this.#transforms.get( UUID );
+			boneTransforms.push( { 
+					UUID,
+					transform: {
+						translation: [ ...transform.translation ],
+						rotation: [ ...transform.rotation ],
+						scale: [ ...transform.scale ],
+					}
+			} );
+		}
+		return boneTransforms;
 	}
 
 	/// bones = [ 
@@ -44,12 +74,18 @@ export default class SkeletonModule extends ModuleCore {
 	///		{ UUID, parent },
 	/// ]
 	setBones ( bones, sync = false ) {
-		console.log(bones)
+		console.log( `SkeletonModule - setBones` );
+
 		for ( const { UUID, parent } of bones ) {
-			// this.#bones.add( bone)
-			console.log( UUID, parent );
 			this.#bones.add( UUID );
 			this.#parents.set( UUID, parent );
+
+			const transform = {
+				translation: [ 0, 0, 0 ],
+				rotation: [ 0, 0, 0, 1 ],
+				scale: [ 1, 1, 1 ],
+			}
+			this.#transforms.set( UUID, transform );
 		}
 
 		this.onChange( this.commands.setBones, { bones: this.bones } );
@@ -59,13 +95,53 @@ export default class SkeletonModule extends ModuleCore {
 		}
 	}
 
-	setTransforms ( boneTransforms, sync = false ) {
+	/// boneTransforms = [ 
+	///		{ UUID, transform },
+	///		{ UUID, transform },
+	///		{ UUID, transform },
+	/// ]
 
+	/// transform = {
+	/// 	translation: [ x, y, z ],
+	/// 	rotation: [ i, j, k, w ],
+	/// 	scale: [ x, y, z ],
+	///}
+	setTransforms ( boneTransforms, sync = false ) {
+		console.log( `SkeletonModule - setTransforms` );
+
+		const updatedUUIDs = [ ];
+
+		for ( const { UUID, transform } of boneTransforms ) {
+			const boneTransform = this.#transforms.get( UUID );
+			if ( boneTransform ) {
+				updatedUUIDs.push( UUID );
+				if ( transform.translation ) {
+					boneTransform.translation.forEach( ( _, i ) => boneTransform.translation[ i ] = transform.translation[ i ] || 0 );
+				}
+				if ( transform.rotation ) {
+					boneTransform.rotation.forEach( ( _, i ) => boneTransform.rotation[ i ] = transform.rotation[ i ] || 0 );
+				}
+				if ( transform.scale ) {
+					boneTransform.scale.forEach( ( _, i ) => boneTransform.scale[ i ] = transform.scale[ i ] || 1 );
+				}
+			}
+		}
+
+		this.onChange( this.commands.setTransforms, { boneTransforms: this.boneTransforms( updatedUUIDs ) } );
+
+		if ( sync ) {
+			this.output( this.commands.setTransforms, { boneTransforms: this.boneTransforms( updatedUUIDs ) } );
+		}
+
+		console.log( updatedUUIDs )
+		console.log( this.boneTransforms( updatedUUIDs ) )
 	}
 
 	getState ( ) {
 		return {
 			...super.getState( ),
+			bones: this.bones,
+			boneTransforms: this.transforms,
 		}
 	}
 
